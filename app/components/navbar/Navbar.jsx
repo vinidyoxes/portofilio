6e72bd9250aeca28f29ecc272e6fb47c65bdcd77
Vinicius <naoprefiro@gmail.com>
2023-08-12 09:59:15 -0300

    sidebar z-index, and fury on sidebar

--- app/components/navbar/Navbar.jsx
+++ app/components/navbar/Navbar.jsx
@@ -106,7 +106,7 @@ function Navbar() {
         <Link href="https://github.com/vinidyoxes/"className="icon"><BsGithub className='size-2em'></BsGithub></Link>
         <Link href="https://www.behance.net/vinidyoxes"className="icon"><Image src='/images/devicon-behance.svg' width="20" height="20"></Image></Link>
         <Link href="https://www.linkedin.com/in/viniciushenrique-silva/"className="icon"><Image src={require('/public/images/skill-icons-linkedin.png')} width="20" height="20"></Image></Link>
-        <h2>adadad</h2>
+        <Link target='_blank' href="https://furyagency.sytes.net/#"className="icon"><Image src='/icons/furyLogo.png' width="20" height="20"></Image></Link>
       </ul>
     </div>
     <div className={`${styles.options}`}>
