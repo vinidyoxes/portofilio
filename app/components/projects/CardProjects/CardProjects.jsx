add17b927f2ec0183bb3949d3c5d17447f99df2f
Vinicius <naoprefiro@gmail.com>
2024-02-23 20:25:40 -0300

    added a project and fixed misclicked project link

--- app/components/projects/CardProjects/CardProjects.jsx
+++ app/components/projects/CardProjects/CardProjects.jsx
@@ -11,21 +11,30 @@ function CardProjects({project}) {
   console.log({})
   return (
 
-        <div className={`${styles.card} group rounded-3xl sm:min-h-300 relative w-500 h-400`}>
-            <div className={styles.overlay}>
-              <div className={styles.content__overlay}>
-                <h3>{project.company}</h3>
-                <p>{project.name}</p>
-                  <div className='flex justify-center'>
-                <Link target='_blank' href={project.viewProject}><Button style='line' icon='true' styled={{color:"white"}}>View Project</Button></Link>
-                <Link target='_blank' href={project.viewDetails}><Button style='line' styled={{color:"white"}}>View Details</Button></Link>
-
-                  </div>
-
-              </div>
-            </div>
-            <Image src={`${project.img}`} layout='fill' className='rounded-lg'></Image>
-       </div>
+       <div className={`${styles.card} group rounded-3xl sm:min-h-300 relative w-500 h-400`}>
+  <div className={styles.overlay}>
+    <div className={styles.content__overlay}>
+      <h3>{project.company}</h3>
+      <p>{project.name}</p>
+      <div className={`flex justify-center ${styles.buttons}` }>
+        {project.viewProject === '#' ? '' : (
+          <Link target='_blank' href={project.viewProject}>
+            <Button style='line' icon='true' styled={{color:"white"}}>
+              View Project
+            </Button>
+          </Link>
+        )}
+        {project.viewDetails ==='#' ? '' : (
+          <Link target='_blank' href={project.viewDetails}>
+            <Button style='line' styled={{color:"white"}}>
+              View Details
+            </Button>
+          </Link>)}
+      </div>
+    </div>
+  </div>
+  <Image src={`${project.img}`} layout='fill' className='rounded-lg' />
+</div>
 
   )
 }
